d1a96d4e1bd0dfa3e2c7b4e57a94229ea707a8f2
jelipo <me@jelipo.com>
2020-08-19 18:29:58 +0800

    update cpu

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -7,47 +7,42 @@ pub struct Arm7 {
 }
 
 impl Arm7 {
-
-
-
     pub fn cond_check(&self, cond: u32) -> bool {
         let z = self.reg.flag_z();
         let c = self.reg.flag_c();
         let v = self.reg.flag_v();
         let n = self.reg.flag_n();
         match cond {
-            /* EQ */
-            0x0 => z,
-            /* NE */
-            0x1 => !z,
-            /* CS */
-            0x2 => c,
-            /* CC */
-            0x3 => !c,
-            /* MI */
-            0x4 => n,
-            /* PL */
-            0x5 => !n,
-            /* VS */
-            0x6 => v,
-            /* VC */
-            0x7 => !v,
-            /* HI */
-            0x8 => c && !z,
-            /* LS */
-            0x9 => !c || z,
-            /* GE */
-            0xA => n == v,
-            /* LT */
-            0xB => n != v,
-            /* GT */
-            0xC => !z && n == v,
-            /* LE */
-            0xD => z || n != v,
-            /* AL */
-            0xE => true,
+            /* EQ */ 0x0 => z,
+            /* NE */ 0x1 => !z,
+            /* CS */ 0x2 => c,
+            /* CC */ 0x3 => !c,
+            /* MI */ 0x4 => n,
+            /* PL */ 0x5 => !n,
+            /* VS */ 0x6 => v,
+            /* VC */ 0x7 => !v,
+            /* HI */ 0x8 => c && !z,
+            /* LS */ 0x9 => !c || z,
+            /* GE */ 0xA => n == v,
+            /* LT */ 0xB => n != v,
+            /* GT */ 0xC => !z && n == v,
+            /* LE */ 0xD => z || n != v,
+            /* AL */ 0xE => true,
             0xF => true, /* reserved, default to execute */
             _ => unreachable!(),
         }
     }
+}
+
+enum InstructionType {
+    BlockDataTransfer,
+    Branch,
+    /// CoprocessorDataTransfer,
+    CPDT,
+    /// CoprocessorDataOperation,
+    CPDO,
+    /// CoprocessorRegisterTransfer,
+    CPRT,
+    /// Software Interrupt
+    SoftInt,
 }
