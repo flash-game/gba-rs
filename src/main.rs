ccf07478414f16a5e4a17eec80fc0ba0429d550a
jelipo <me@jelipo.com>
2020-08-20 18:27:09 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ mod cpu;
 
 fn main() {
     println!("Hello, world!");
-    let a = 0;
-    a⌃1
+    let a: u32 = (u32::max_value() & 0b_0000_0000_1111_1111_1111_1111_1111_1111) - 100;
+    println!("{:32b}", a);
+
+    println!("{}", a as i32);
+
+    println!("{:32b}", a << 2);
+
+    println!("{:32b}", (a << 2) as i32);
+
+    println!("{:32b}", ((a << 8) as i32 >> 6) as u32);
 }
