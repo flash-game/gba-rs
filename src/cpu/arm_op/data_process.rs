d164cb8aa1a2dc3cf478a88c6668aa236c771ef0
jelipo <me@jelipo.com>
2022-03-17 00:46:04 +0800

    update

--- src/cpu/arm_op/data_process.rs
+++ src/cpu/arm_op/data_process.rs
@@ -50,9 +50,11 @@ impl DataProcess {
         }
     }
 
-    fn add(set_cond: bool) {}
+    fn add(set_cond: bool, op: u32, reg: &mut Register) {
+        let (operand1, operand2, rd) = DataProcess::base_info(op, reg);
+    }
 
-    fn base_info(op: u32, reg: &mut Register) -> InternalBase {
+    fn base_info(op: u32, reg: &mut Register) -> (u32, u32, u8) {
         let rn = op.extract(16, 4) as u8;
         let operand1 = reg.reg_val(rn);
         let rd = op.extract(12, 4) as u8;
@@ -63,21 +65,22 @@ impl DataProcess {
         } else {
             let rm = (op & 0b1111) as u8;
             let rm_val = reg.reg_val(rm);
-            let shift_type = op.extract(5, 2);
-            if op.get_bit_bool(4) {
+            let shift_type = op.extract(5, 2) as u8;
+            let shift_amount = if op.get_bit_bool(4) {
                 let rs = op.extract(8, 4) as u8;
-                let rs_val = reg.reg_val(rs) as u8 & 0b00011111;
+                reg.reg_val(rs) & 0b11111
             } else {
-                let shift_amount = op.extract(7, 5) as u8;
+                op.extract(7, 5)
+            };
+            match shift_type {
+                0b00 => rm_val << shift_amount,
+                0b01 => rm_val >> shift_amount,
+                0b10 => ((rm_val as i32) >> shift_amount) as u32,
+                0b11 => rm_val.rotate_right(shift_amount),
+                _ => unreachable!()
             }
-            1
-            // 寄存器移位
         };
-        InternalBase {
-            operand1,
-            operand2,
-            rd,
-        }
+        (operand1, operand2, rd)
     }
 }
 
