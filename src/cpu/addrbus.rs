6d2bd5ac87d46df6eaa90343b03c369f2d407f49
jelipo <me@jelipo.com>
2022-03-23 00:48:07 +0800

    update

--- src/cpu/addrbus.rs
+++ src/cpu/addrbus.rs
@@ -37,16 +37,54 @@ impl Memory for RoMem {
 
     fn get_half_word(&self, addr: u32) -> u16 {
         let start = (addr - self.offset) as usize;
-
-        let b = (&self.data[start..(start + 2)]).try_into()?;
-        u16::from_le_bytes(*b)
+        let b: [u8; 2] = self.data[start..(start + 2)].try_into().unwrap();
+        u16::from_le_bytes(b)
     }
 
     fn get_word(&self, addr: u32) -> u32 {
-        todo!()
+        let start = (addr - self.offset) as usize;
+        let b: [u8; 4] = self.data[start..(start + 4)].try_into().unwrap();
+        u32::from_le_bytes(b)
     }
 
     fn set_byte(&self, addr: u32, val: u8) {}
     fn set_half_word(&self, addr: u32, val: u16) {}
     fn set_word(&self, addr: u32, val: u32) {}
+}
+
+struct RwMem {
+    data: Vec<u8>,
+    offset: u32,
+}
+
+impl Memory for RwMem {
+    fn get_byte(&self, addr: u32) -> u8 {
+        self.data[addr - self.offset]
+    }
+
+    fn get_half_word(&self, addr: u32) -> u16 {
+        let start = (addr - self.offset) as usize;
+        let b: [u8; 2] = self.data[start..(start + 2)].try_into().unwrap();
+        u16::from_le_bytes(b)
+    }
+
+    fn get_word(&self, addr: u32) -> u32 {
+        let start = (addr - self.offset) as usize;
+        let b: [u8; 4] = self.data[start..(start + 4)].try_into().unwrap();
+        u32::from_le_bytes(b)
+    }
+
+    fn set_byte(&mut self, addr: u32, val: u8) {
+        self.data[(addr - self.offset) as usize] = val
+    }
+    fn set_half_word(&mut self, addr: u32, val: u16) {
+        let u16_bytes: [u8; 2] = val.to_le_bytes();
+        self.data[addr] = u16_bytes[0];
+        self.data[addr + 1] = u16_bytes[1];
+    }
+    fn set_word(&mut self, addr: u32, val: u32) {
+        let u32_bytes: [u8; 4] = val.to_le_bytes();
+        let addr = addr as usize;
+        self.data[addr..addr + 4].copy_from_slice(&u32_bytes);
+    }
 }
--- src/cpu/mem.rs
+++ src/cpu/mem.rs
@@ -18,16 +18,16 @@ pub trait Memory {
     //---------------------SET-----------------------
 
     /// Set a byte from a 32-bit address
-    fn set_byte(&self, addr: u32, val: u8);
+    fn set_byte(&mut self, addr: u32, val: u8);
 
     /// Set a half-word from a 32-bit address
-    fn set_half_word(&self, addr: u32, val: u16) {
+    fn set_half_word(&mut self, addr: u32, val: u16) {
         self.set_byte(addr, val as u8);
         self.set_byte(addr + 1, (val >> 8) as u8)
     }
 
     /// Set a word from a 32-bit address
-    fn set_word(&self, addr: u32, val: u32) {
+    fn set_word(&mut self, addr: u32, val: u32) {
         self.set_half_word(addr, val as u16);
         self.set_half_word(addr + 1, (val >> 16) as u16)
     }
