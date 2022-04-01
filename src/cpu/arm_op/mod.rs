e7362fe2dedeaca0f19d129402dc4a31f2a712b7
jelipo <me@jelipo.com>
2022-04-02 00:28:05 +0800

    update shifter

--- src/cpu/arm_op/mod.rs
+++ src/cpu/arm_op/mod.rs
@@ -1,6 +1,8 @@
-use crate::cpu::reg::Register;
-use fantasy_util::bit::usize::BitUtil;
 use std::ops::Shl;
+
+use fantasy_util::bit::usize::BitUtil;
+
+use crate::cpu::reg::Register;
 
 pub mod branch;
 pub mod branch_exchange;
@@ -46,10 +48,16 @@ pub mod undefined;
 /// ```
 ///
 /// ### 指令指定移位量
-/// 当指令中指定移位量时，它包含在一个 5 位字段中，可以取 0 到 31 之间的任何值。逻辑左移 (LSL) 取 Rm 的内容并将每个位移动指定的量到 更显着的地位。
-/// 结果的最低位用零填充，Rm 中没有映射到结果的高位被丢弃，除了最低位丢弃的位成为移位器进位输出，可以锁存到 C 位 当 ALU 操作在逻辑类中时的 CPSR（见上文）。
+/// 当指令中指定移位量时，它包含在一个 5 位字段中，可以取 0 到 31 之间的任何值。
+/// 逻辑左移 (LSL) 取 Rm 的内容并将每个位移动指定的量到更高位。结果的最低位用零填充，Rm 中没有映射到结果的高位被丢弃，除了最低位丢弃的位成为移位器进位输出，可以锁存到 C 位 当 ALU 操作在逻辑类中时的 CPSR（见上文）。
 /// 例如，LSL#5 的效果如图 4-6：逻辑左移
-
+/// ###
+/// LSL #0 是一种特殊情况，其中移位器进位是 CPSR C 标志的旧值。 Rm 的内容直接用作第二个操作数。
+/// 逻辑右移 (LSR) 类似，但 Rm 的内容在结果中移动到不太重要的位置。 LSR #5 的效果如➲图 4-7：逻辑右移
+/// ###
+/// 预期对应于 LSR #0 的移位字段的形式用于编码 LSR #32，其结果为零，Rm 的第 31 位作为进位输出。
+/// 逻辑右移零是多余的，因为它与逻辑左移零相同，因此汇编器会将 LSR #0（以及 ASR #0 和 ROR #0）转换为 LSL #0，并允许指定 LSR #32。
+/// 算术右移 (ASR) 类似于逻辑右移，不同之处在于高位用 Rm 的第 31 位而不是零填充。 这保留了 2 的补码表示法中的符号。 例如，ASR #5 显示在➲图 4-8：算术右移。
 pub fn barrel_shifter(instruct: u32, reg: &mut Register) {
     let rm = (instruct & 0b1111) as u8;
     let rm_val = reg.reg_val(rm);
@@ -67,4 +75,14 @@ pub fn barrel_shifter(instruct: u32, reg: &mut Register) {
         0b11 => rm_val.rotate_right(shift_amount),
         _ => unreachable!(),
     };
+}
+
+/// 逻辑左移
+fn logical_left(rm_val: u32, shift_amount: u32, reg: &mut Register) -> (u32, bool) {
+    let result = rm_val << shift_amount;
+    match shift_amount {
+        0 => (result, reg.cpsr.flag_c()),
+        32 => (result, rm_val.get_bit_bool(31)),
+        _ => (result, result >> shift_amount == rm_val)
+    }
 }
